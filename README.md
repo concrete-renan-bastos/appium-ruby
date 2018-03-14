55057a0e7d3be5f4364080a5ddce164db9e74dd7
Renan Bastos <renan.bastos@concrete.com.br>
2018-03-14 15:12:31 -0300

    mudanças dos teste android e adaptação ao zenfone

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Projeto de testes utilizando Appium para iOS e Android
+
+Teste de android usando o app flexbox que se encontra no link abaixo
+
+https://github.com/google/flexbox-layout
--- android/features/support/env.rb
+++ android/features/support/env.rb
@@ -2,6 +2,8 @@ require 'appium_lib'
 require 'cucumber'
 require 'capybara'
 require 'touch_action'
+require 'rspec'
+
 
 desired_caps = Appium.load_appium_txt file: File.expand_path('./../../../../appium_android.txt', __FILE__), verbose: true
 driver = Appium::Driver.new(desired_caps, true)
